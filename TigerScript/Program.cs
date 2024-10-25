49bd30d2ff6823999b3a027351cc446b795d7a69
Djurinschi Andrei <djurinschi.andreiai@gmail.com>
2024-10-25 03:11:03 +0300

    SBlocks and permutation functions

--- TigerScript/Program.cs
+++ TigerScript/Program.cs
@@ -1,1 +1,62 @@
-﻿
+﻿using System;
+
+
+public class TigerHash 
+{
+    private static readonly byte[] H0 =
+    [
+        0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF
+    ];
+
+    private static readonly byte[] H1 =
+    [
+        0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10
+    ];
+
+    private static readonly byte[] H2 =
+    [
+        0xF0, 0x96, 0xA5, 0xB4, 0xC3, 0xB2, 0xE1, 0x87
+    ];
+
+    private static readonly byte[][] H = { H0, H1, H2 };
+
+    private static readonly int[] _sBlocks = { 0xB1, 0xA2, 0xC4, 0xF6, 0x8D, 0xE3, 0xC3, 0xD7 };
+
+
+    public static void Main(string[] args) {
+        string myWord = "ADCRYPTO";
+        byte[] stringToDecimal = System.Text.Encoding.UTF8.GetBytes(myWord);
+        Console.WriteLine("Байты: " + ToHex(stringToDecimal));
+        byte[] SBlocksResult = SBlocks(stringToDecimal);
+        Console.WriteLine("S Blocks " + ToHex(SBlocksResult));
+        byte[] permutation = Permutation(SBlocksResult);
+        Console.WriteLine("Результат после перестановок: " + ToHex(permutation));
+        
+    }
+
+    private static string ToHex(byte[] input) 
+    {
+        return BitConverter.ToString(input).Replace("-", " ");
+    }
+
+    private static byte[] SBlocks(byte[] input) 
+    {
+        byte[] result = new byte[input.Length];
+        for (int i = 0; i < input.Length; i++) 
+        {
+            result[i] = (byte)_sBlocks[i];
+        }
+        return result;
+    
+    }
+
+    private static byte[] Permutation(byte[] input) 
+    {
+        byte[] result = new byte[input.Length];
+        int[] permutationOrder = {2,3,1,4,0,6,7,5};
+        for (int i = 0; i < permutationOrder.Length; i++) {
+            result[i] = input[permutationOrder[i]];
+        }
+        return result;
+    }
+}

d570949b72ae441f34573438b6c4f268e956ad3d
Djurinschi Andrei <djurinschi.andreiai@gmail.com>
2024-10-26 00:39:29 +0300

    Final

--- TigerScript/Program.cs
+++ TigerScript/Program.cs
@@ -26,25 +26,17 @@ public class TigerHash
     public static void Main(string[] args) {
         string myWord = "ADCRYPTO";
         byte[] stringToDecimal = System.Text.Encoding.UTF8.GetBytes(myWord);
-        Console.WriteLine("Bytes: " + ToHex(stringToDecimal));
         Console.WriteLine("Байты: " + ToHex(stringToDecimal));
         byte[] SBlocksResult = SBlocks(stringToDecimal);
         Console.WriteLine("S Blocks " + ToHex(SBlocksResult));
         byte[] permutation = Permutation(SBlocksResult);
         Console.WriteLine("Результат после перестановок: " + ToHex(permutation));
-        byte[] afterShift = BitShift(permutation);
-        Console.WriteLine("Результат после сдвига: " + ToHex(afterShift));
+        
     }
 
-    private static string[] ToHex(byte[] input) 
+    private static string ToHex(byte[] input) 
     {
-        string[] result = new string[input.Length];
-        for (int i = 0; i < input.Length; i++) 
-        {
-            int ascciValue = (int)input[i];
-            result[i] = input[i].ToString("X2");
-        }
-        return result;
+        return BitConverter.ToString(input).Replace("-", " ");
     }
 
     private static byte[] SBlocks(byte[] input) 
@@ -64,16 +56,6 @@ public class TigerHash
         int[] permutationOrder = {2,3,1,4,0,6,7,5};
         for (int i = 0; i < permutationOrder.Length; i++) {
             result[i] = input[permutationOrder[i]];
-        }
-        return result;
-    }
-
-    private static byte[] BitShift(byte[] input) 
-    {
-        byte[] result = new byte[input.Length];
-        for (int i = 0; i < input.Length; i++) 
-        {
-            result[i] = (byte)(input[i] << 2);
         }
         return result;
     }
